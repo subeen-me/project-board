2c2c941319acc8cb41effe27f07b74c3594514d3
subeen-me <subeen.me@gmail.com>
2023-03-05 01:39:14 +0900

    #25 - 도메인 업데이트: UserAccount 회원 ID에 유니크 키 추가
    회원 id로 로그인하고 유저 식별하므로, 당연히 uk여야 한다.
    이 부분이 설계에서 반영되지 않았던 것을 발견하여 추가하였다.

--- project-board/src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ project-board/src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -58,7 +58,7 @@ class JpaRepositoryTest {
 
         // Given
         long previousCount = articleRepository.count();
-        UserAccount userAccount = userAccountRepository.save(UserAccount.of("susoo", "pw", null, null, null));
+        UserAccount userAccount = userAccountRepository.save(UserAccount.of("newSusoo", "pw", null, null, null));
 
         Article article = Article.of(userAccount, "new article", "new content", "#spring");
 
