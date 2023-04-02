da63940fd8bb064970106406e120d143e235f1e4
subeen-me <subeen.me@gmail.com>
2023-04-02 23:01:36 +0900

    #29 - 해시태그 검색 페이지를 위한 기능 구현
    해시태그 뷰에 모든 게시글에서 나오는 해시태그를
    정리해서 보여주는 기능을 넣기 위해,
    해시태그만 뽑아서 unique 하게 보여주는 쿼리를
    querydsl 로 작성함
    그 외 서비스 코드, 컨트롤러, 관련 테스트 작성

---
+++ project-board/src/main/java/com/fastcampus/projectboard/repository/querydsl/ArticleRepositoryCustomImpl.java
@@ -0,0 +1,28 @@
+package com.fastcampus.projectboard.repository.querydsl;
+
+import com.fastcampus.projectboard.domain.Article;
+import com.fastcampus.projectboard.domain.QArticle;
+import com.querydsl.jpa.JPQLQuery;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+public class ArticleRepositoryCustomImpl extends QuerydslRepositorySupport implements ArticleRepositoryCustom{
+
+
+    public ArticleRepositoryCustomImpl() {
+        super(Article.class);
+    }
+
+    @Override
+    public List<String> findAllDistinctHashtags() {
+        QArticle article = QArticle.article;
+
+        JPQLQuery<String> query = from(article)
+                .distinct()
+                .select(article.hashtag)
+                .where(article.hashtag.isNotNull());
+
+        return query.fetch();
+    }
+}
--- project-board/src/main/java/com/fastcampus/projectboard/service/ArticleService.java
+++ project-board/src/main/java/com/fastcampus/projectboard/service/ArticleService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -72,4 +73,15 @@ public class ArticleService {
         return articleRepository.count();
     }
 
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticlesViaHashtag(String hashtag, Pageable pageable) {
+        if(hashtag == null || hashtag.isBlank()) { //해시태그 페이지는 검색어가 없으면 빈 화면, 검색했을 때만 검색한 해시태그 보여준다.
+            return Page.empty(pageable);
+        }
+        return articleRepository.findByHashtag(hashtag, pageable).map(ArticleDto::from);
+    }
+
+    public List<String> getHashtags() {
+        return articleRepository.findAllDistinctHashtags();
+    }
 }
